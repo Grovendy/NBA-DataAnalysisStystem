3701c281c76eb45b5a559d3a643d966dffd0b5fd
wyjend <50990500+wyjend@users.noreply.github.com>
2022-06-02 20:41:29 +0800

    Add files via upload

---
+++ UI/src/main/java/vo/PlayerInfoVO.java
@@ -0,0 +1,80 @@
+package vo;
+
+import java.io.Serializable;
+
+/**
+ * PlayerInfo数据VO类
+ *
+ */
+public class PlayerInfoVO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 姓名
+	 */
+	public String name;
+	
+	/**
+	 * 出生年月
+	 */
+	public String born;
+	
+	/**
+	 * 出生地
+	 */
+	public String hometown;
+	
+	/**
+	 * 位置
+	 */
+	public String position;
+	
+	/**
+	 * 身高
+	 */
+	public String height;
+	
+	/**
+	 * 体重
+	 */
+	public Integer weight;
+	
+	/**
+	 * shoots
+	 */
+	public String shoots;
+	
+	/**
+	 * 高中
+	 */
+	public String high_school;
+	
+	/**
+	 * 大学
+	 */
+	public String college;
+	
+	/**
+	 * NBA Draft
+	 */
+	public String draft;
+	
+	/**
+	 * NBA Debut 初次登场
+	 */
+	public String debut;
+	
+	/**
+	 * NBA球龄（经验）
+	 */
+	public Integer exp;
+	
+	/**
+	 * 球衣号
+	 */
+	public Integer number;
+	
+	public PlayerInfoVO(){}
+	
+}
---
+++ UI/src/main/java/vo/TeamAdvancedVO.java
@@ -0,0 +1,137 @@
+package vo;
+
+import java.io.Serializable;
+
+/**
+ * TeamAdvanced数据VO类
+ *
+ */
+public class TeamAdvancedVO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 球队
+	 */
+	public String abbr;
+	
+	/**
+	 * 赛季
+	 */
+	public String season;
+	
+	/**
+	 * PW(wins_pyth)
+	 * Pythagorean wins, i.e., expected wins based on points scored and allowed
+	 */
+	public Double pw;
+	
+	/**
+	 * PL(losses_pyth)
+	 * Pythagorean losses, i.e., expected losses based on points scored and allowed
+	 */
+	public Double pl;
+	
+	/**
+	 * MOV =Margin of Victory
+	 */
+	public Double mov;
+	
+	/**
+	 * SOS = Strength of Schedule
+	 * A rating of strength of schedule. The rating is denominated in points above/below average,
+	 * where zero is average.
+	 */
+	public Double sos;
+	
+	/**
+	 * SRS = Simple Rating System
+	 * A team rating that takes into account average point differential and strength of schedule. 
+	 * The rating is denominated in points above/below average, where zero is average.
+	 */
+	public Double srs;
+	
+	/**
+	 * ORtg = Offensive Rating
+	 * An estimate of points produced (players) or scored (teams) per 100 possessions
+	 */
+	public Double off_rtg;
+	
+	/**
+	 * DRtg = Defensive Rating
+	 * An estimate of points allowed per 100 possessions
+	 */
+	public Double def_rtg;
+	
+	/**
+	 * Pace = Pace Factor
+	 * An estimate of possessions per 48 minutes
+	 */
+	public Double pace;
+	
+	/**
+	 * Free Throw Attempt Rate
+	 */
+	public Double fta_per_fga_pct;
+	
+	/**
+	 * 3-Point Attempt Rate
+	 */
+	public Double fg3a_per_fga_pct;
+	
+	/**
+	 * 进攻-Effective Field Goal Percentage
+	 */
+	public Double off_efg_pct;
+	
+	/**
+	 * 进攻-Turnover Percentage
+	 */
+	public Double off_tov_pct;
+	
+	/**
+	 * 进攻-进攻篮板率
+	 */
+	public Double orb_pct;
+	
+	/**
+	 * 进攻-Free Throws Per Field Goal Attempt
+	 */
+	public Double off_ft_rate;
+	
+	/**
+	 * 防守-Effective Field Goal Percentage
+	 */
+	public Double opp_efg_pct;
+	
+	/**
+	 * 防守-Turnover Perce
+	 */
+	public Double opp_tov_pct;
+	
+	/**
+	 * 防守-防守篮板率
+	 */
+	public Double drb_pct;
+	
+	/**
+	 * 防守-Opponent Free Throw per Field Goal Attempt
+	 */
+	public Double opp_ft_rate;
+	
+	/**
+	 * Arena_name
+	 */
+	public String arena;
+	
+	/**
+	 * attendance
+	 */
+	public Integer attendance;
+	
+	/**
+	 * 无参构造函数
+	 */
+	public TeamAdvancedVO(){}
+	
+}
