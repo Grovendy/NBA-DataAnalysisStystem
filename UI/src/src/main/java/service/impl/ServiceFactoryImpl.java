ac020a1213326eed0ca7e38b424243a751456d46
wyjend <50990500+wyjend@users.noreply.github.com>
2022-06-02 20:28:19 +0800

    Add files via upload

---
+++ UI/src/src/main/java/service/impl/ServiceFactoryImpl.java
@@ -0,0 +1,105 @@
+package service.impl;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Properties;
+
+import service.CommonService;
+import service.InferStatsService;
+import service.MatchService;
+import service.PlayerService;
+import service.ServiceFactory;
+import service.StatsService;
+import service.TeamService;
+
+/**
+ * RMI service 服务工厂
+ *
+ */
+public class ServiceFactoryImpl extends UnicastRemoteObject implements ServiceFactory{
+
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * 单例实例
+	 */
+	private static ServiceFactory serviceFactory = null;
+	
+	/**
+	 * 默认ip
+	 */
+	public static String address = "localhost";
+	
+	/**
+	 * 默认端口
+	 */
+	public static String port = "8888";
+
+    static{
+    	Properties prop = new Properties();
+    	try{
+    		InputStream in = new BufferedInputStream(new FileInputStream("service.properties"));
+    		prop.load(in);
+    		address = prop.getProperty("address");
+    		port = prop.getProperty("port");
+    	}catch(Exception e){
+    		System.out.println(e);
+    	}
+    }
+    
+	/**
+	 * 私有化构造方法
+	 * @throws RemoteException
+	 */
+	private ServiceFactoryImpl() throws RemoteException {
+		super();
+	}
+	
+	/**
+	 * 获得实例
+	 * @return
+	 */
+	public static ServiceFactory getInstance() {
+		try{
+			serviceFactory = (ServiceFactory)Naming.lookup("rmi://"+address+":"+port+"/ServiceFactory");
+		}catch(RemoteException | NotBoundException |MalformedURLException e){
+			e.printStackTrace();
+			System.out.println("\n-------- Get Service Error ------ ");
+		}
+		return serviceFactory;
+	}
+
+	public PlayerService getPlayerService() throws RemoteException {
+		return new PlayerServiceImpl();
+	}
+
+	public TeamService getTeamService() throws RemoteException {
+		return new TeamServiceImpl();
+	}
+
+	public MatchService getMatchService() throws RemoteException {
+		return new MatchServiceImpl();
+	}
+
+	public CommonService getCommonService() throws RemoteException {
+		return new CommonServiceImpl();
+	}
+
+	@Override
+	public StatsService getStatsService() throws RemoteException {
+		return new StatsServiceImpl();
+	}
+
+	@Override
+	public InferStatsService getInferStatsService() throws RemoteException {
+		return new InferStatsServiceImpl();
+	}
+
+
+}
---
+++ UI/src/src/main/java/vo/TeamOppPerGameVO.java
@@ -0,0 +1,137 @@
+package vo;
+
+import java.io.Serializable;
+
+/**
+ * TeamOppPerGameVO类
+ *
+ */
+public class TeamOppPerGameVO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 球队（缩写）
+	 */
+	public String abbr;
+	
+	/**
+	 * 赛季
+	 */
+	public String season;
+	
+	/**
+	 * 时间
+	 */
+	public Double minute;
+	
+	/**
+	 * 投篮命中数
+	 */
+	public Double fg;
+	
+	/**
+	 * 投篮出手数
+	 */
+	public Double fga;
+	
+	/**
+	 * 投篮命中率
+	 */
+	public Double fga_pct;
+	
+	/**
+	 * 三分命中数 
+	 */
+	public Double fg3;
+	
+	/**
+	 * 三分出手数
+	 */
+	public Double fg3a;
+	
+	/**
+	 * 三分命中率
+	 */
+	public Double fg3_pct;
+	
+	/**
+	 * 两分命中数
+	 */
+	public Double fg2;
+	
+	/**
+	 * 两分投篮数
+	 */
+	public Double fg2a;
+	
+	/**
+	 * 两分命中率
+	 */
+	public Double fg2_pct;
+	
+	/**
+	 * 罚球命中数
+	 */
+	public Double ft;
+	
+	/**
+	 * 罚球出手数
+	 */
+	public Double fta;
+	
+	/**
+	 * 罚球命中率
+	 */
+	public Double ft_pct;
+	
+	/**
+	 * 进攻篮板
+	 */
+	public Double orb;
+	
+	/**
+	 * 防守篮板
+	 */
+	public Double drb;
+	
+	/**
+	 * 总篮板
+	 */
+	public Double trb;
+	
+	/**
+	 * 助攻
+	 */
+	public Double ast;
+	
+	/**
+	 * 抢断
+	 */
+	public Double stl;
+	
+	/**
+	 * 盖帽
+	 */
+	public Double blk;
+	
+	/**
+	 * 失误
+	 */
+	public Double tov;
+	
+	/**
+	 * 犯规
+	 */
+	public Double pf;
+	
+	/**
+	 * 得分
+	 */
+	public Double pts;
+	
+	/**
+	 * 无参构造函数
+	 */
+	public TeamOppPerGameVO(){}
+}
